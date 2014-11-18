e39415ad73582ba0441c8c0d2b00717afcffb2ef
Stefan Arentz <stefan@arentz.ca>
2014-11-17 19:32:24 -0500

    GreetingController can now return json and text.

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,10 +1,7 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController
@@ -12,13 +9,14 @@ public class GreetingController
     @Autowired
     private Greeter greeter;
 
-//    @RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = "application/json")
-//    public Greeting greetToJson(@RequestParam(required = true) String name) {
-//        return new Greeting(42, "Hello, " + name);
-//    }
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Greeting greetToJson(@RequestParam(required = true) String name) {
+        return new Greeting(42, "Hello, " + name);
+    }
 
-    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-    public String greetToText(@RequestParam(required = true) String name) {
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = "text/plain")
+    public @ResponseBody String greetToText(@RequestParam(required = true) String name) {
         return greeter.greet(name);
     }
 }
